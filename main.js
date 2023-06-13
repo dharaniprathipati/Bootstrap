ba64ab10db622551d9aa9c9dc073e098969bebd4
dharaniprathipati <dharaniprathipati@2002gmail.com>
2023-06-13 11:38:54 +0530

    new

---
+++ main.js
@@ -0,0 +1,30 @@
+
+ //   const x=20;
+//   const x=20;
+//   console.log(x);
+
+
+  {
+    let x=50;
+    console.log(x);
+  }
+
+  var y=30;
+  console.log(y);
+  document.write(y);
+
+  window.alert("Home");
+  document.querySelector("#active").innerHTML="Microsoft";
+  document.querySelector(".office").innerHTML="Student";
+
+//  javascript cases
+ const firstName="Dharani";
+ document.querySelector("#para").innerHTML= firstName;
+
+// operators
+// Arithmetic
+  var x=100;
+  var y=50;
+  var z=x/y;
+  console.log(z);
+  
